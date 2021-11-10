f8367898cb2bc7dbec6d17f4e23b3d754b300670
Cagatay <cagataybayramgonen@gmail.com>
2021-11-10 16:22:56 +0300

    Hooks

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,16 +6,18 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin ={"html:target\\cucumber-reports.html"},
+        plugin = {"html:target\\cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
         tags = "@EDTMultiParam", // feature'da scenariolara tagler atayabiliriz.
         // atanan taglari burada yazarak calistirabiliriz.
 
         dryRun = false
-            // dryRun=true --> testi calistirmadan bize eksik adimi verir. testi calistirmaz.
-            // dryRun=false --> testi calistirir, eksik adimla karsilasirsa test failed olur
-            // ve bize eksik adimi raporlar.
+        // dryRun=true --> testi calistirmadan bize eksik adimi verir. testi calistirmaz.
+        // dryRun=false --> testi calistirir, eksik adimla karsilasirsa test failed olur
+        // ve bize eksik adimi raporlar.
 )
 
 public class Runner {
